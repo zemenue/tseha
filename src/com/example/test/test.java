e6c8ba13838409f0097df083e2b0def6ab8326d3
zemenu endale <zemenue@gmail.com>
2023-02-06 21:51:30 +0300

    bn

--- src/com/example/test/test.java
+++ src/com/example/test/test.java
@@ -1,19 +1,12 @@
 package com.example.test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import com.example.Logic.Login;
 
 public class test {
-  static Connection conn = null;
+ 
 
-  public static void main(String[] args) {
-    Login l = new Login();
+ /*  public static void main(String[] args) {
+   
     
-  }
+  } */
 
 }
