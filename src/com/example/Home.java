c7cbb99cf62b910aad98ebb0ddbb4f7c5e1b6c08
zemenu endale <zemenue@gmail.com>
2023-02-09 13:44:37 +0300

    max

--- src/com/example/Home.java
+++ src/com/example/Home.java
@@ -6,6 +6,7 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.beans.PropertyVetoException;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -82,11 +83,20 @@ public class Home {
     footer.add(footer_text, BorderLayout.EAST);
 
     register.setVisible(true);
+
     desktopPane.add(register);
+
     frame.setJMenuBar(createMenuBar());
     frame.add(desktopPane, BorderLayout.CENTER);
     frame.add(footer, BorderLayout.PAGE_END);
+    try {
+      register.setMaximum(true);
+    } catch (PropertyVetoException e1) {
+      // TODO Auto-generated catch block
+      System.out.println(e1.getMessage());
+    }
     frame.setSize(1000, 800);
+    frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
     frame.setVisible(true);
 
   }
